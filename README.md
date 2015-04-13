e49a8fd31b86e6577f2801b3a5ccd3c57c06931b
Steve Kinney <hello@stevekinney.net>
2015-04-13 08:54:04 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Falling Circles
+
+A first stab at experimenting with [p5.js](http://p5js.org/).
+
+Totally experimental.
---
+++ sketch.js
@@ -0,0 +1,12 @@
+var width, height, droplets;
+
+function setup() {
+  width = 640;
+  height = 480;
+  createCanvas(width, height);
+  noStroke();
+}
+
+function draw() {
+  background(255);
+}
